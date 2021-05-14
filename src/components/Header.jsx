eea936d888e1ed5b23966479aeda638e348e523a
Ashish-1062 <21516ashish@gmail.com>
2021-05-14 21:52:19 +0530

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+	return (
+		<div className="header">
+			<img
+				className="header-logo"
+				src="https://1000logos.net/wp-content/uploads/2017/02/Instagram-Logo.png"
+				alt=""
+			></img>
+		</div>
+	);
+}
+
+export default Header;
---
+++ src/components/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Post.css";
+function Post() {
+	return (
+		<div className="post">
+			<h3>username</h3>
+			<img
+				className="post-img"
+				src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-chevrolet-corvette-z06-1607016574.jpg?crop=0.737xw:0.738xh;0.181xw,0.218xh&resize=640:*"
+				alt=""
+			></img>
+		</div>
+	);
+}
+
+export default Post;
